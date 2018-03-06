e3eb2d251aa32b8cccc010f1101463fa632fd793
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-03-06 15:15:27 -0500

    First user migration complete

--- db/migrate/20180306192820_create_users.rb
+++ db/migrate/20180306192820_create_users.rb
@@ -1,12 +1,17 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.string :username
-      t.string :password_digest
-      t.string :session_token
-      t.string :e-mail
-
+      t.string :username, null: false
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
+      t.string :e_mail
       t.timestamps
     end
+    add_index :users, :username, unique: true
+    add_index :users, :e_mail, unique: true
   end
+
+
+
+
 end
