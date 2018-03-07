2050c70f8f7d23b4cae2c39f9ee6106f52e26a37
App Academy Student <alexander_podolsky@alumni.brown.edu>
2018-03-06 21:27:19 -0500

    Auth ajax actions defined

---
+++ frontend/entry.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from './components/root';
+import {login} from './util/session_api_util';
+// import configureStore from './store/store';
+
+
+
+// document.addEventListener('DOMContentLoaded',()=>{
+//   // const store = configureStore();
+//   const root = document.getElementById('root');
+//   ReactDOM.render(<Root store={store}></Root>,root);
+//
+// });
+
+
+window.login = login;
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+  ReactDOM.render(<h1>Welcome to BuenosLeídos</h1>, root);
+});
---
+++ frontend/store/store.js
@@ -0,0 +1,14 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+// import rootReducer
+//
+//
+// const configureStore = (preloadedState = {}) => (
+//   createStore(rootReducer)
+//
+//
+//
+//
+//
+// );
