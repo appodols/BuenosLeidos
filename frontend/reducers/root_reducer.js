ca3278afdbcf6d47f9fb3b01e3e27f957f50a7d4
appodols <alexander_podolsky@alumni.brown.edu>
2018-03-07 14:14:29 -0500

    Root Reducer complete

---
+++ frontend/reducers/root_reducer.js
@@ -0,0 +1,13 @@
+/*jshint esversion: 6 */
+import { combineReducers } from 'redux';
+import errorsReducer from './errors_reducer';
+import sessionReducer from './session_reducer';
+
+
+const rootReducer = combineReducers({
+  session: sessionReducer,
+  errors: errorsReducer
+});
+
+
+export default rootReducer;
