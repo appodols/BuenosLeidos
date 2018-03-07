fe0af63dfe2bb9ffd87f357e14fa443c6a886f73
appodols <alexander_podolsky@alumni.brown.edu>
2018-03-07 14:25:14 -0500

    Store created

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,14 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-// import rootReducer
-//
-//
-// const configureStore = (preloadedState = {}) => (
-//   createStore(rootReducer)
-//
-//
-//
-//
-//
-// );
+import rootReducer from '../reducers/root_reducer';
+
+
+const configureStore = (preloadedState = {}) => (
+  createStore(rootReducer,preloadedState,applyMiddleware(thunk, logger)
+  )
+);
+
+
+export default configureStore;
