a848df04a4e5901457535c63f1b72071ef7b8c27
appodols <alexander_podolsky@alumni.brown.edu>
2018-04-12 16:16:27 -0400

    bookshelf ownership-actions complete

---
+++ frontend/actions/bookshelf_ownership_actions.js
@@ -0,0 +1,35 @@
+/*jshint esversion: 6 */
+import * as ApiUtils from '../util/bookshelf_ownership_api_util';
+export const RECEIVE_BOOKSHELF = 'RECEIVE_BOOKSHELF';
+export const RECEIVE_BOOKSHELF_OWNERSHIP_ERRORS = 'RECEIVE_BOOKSHELF_ERRORS';
+
+
+
+export const createBookShelfOwnership = (bookShelfOwnership) => dispatch => (
+  ApiUtils.createBookShelfOwnership(bookShelfOwnership).then(bookShelf=>(
+    dispatch(receiveBookShelf(bookShelf))
+  ), error => (
+    dispatch(receiveError(error.responseJSON))
+  ))
+);
+
+export const removeBookShelfOwnership = (id) => dispatch => (
+  ApiUtils.deleteBookshelfOwnership(id).then(bookShelf=>(
+    dispatch(receiveBookShelf(bookShelf))
+  ), error => (
+    dispatch(receiveError(error.responseJSON))
+  ))
+);
+
+
+export const receiveBookShelf = bookshelf => ({
+  type: RECEIVE_BOOKSHELF,
+  bookshelf
+});
+
+
+
+export const receiveError = error => ({
+  type: RECEIVE_BOOKSHELF_OWNERSHIP_ERRORS,
+  error
+});
