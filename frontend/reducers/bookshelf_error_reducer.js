0a16a8f87be3c4f7395fd4e6901cdfe2a83be3af
appodols <alexander_podolsky@alumni.brown.edu>
2018-04-12 12:53:41 -0400

    bookshelf-error-reducer complete

---
+++ frontend/reducers/bookshelf_error_reducer.js
@@ -0,0 +1,16 @@
+/*jshint esversion: 6 */
+import React from 'react';
+import merge from 'lodash/merge';
+import { RECEIVE_BOOKSHELF_ERRORS } from '../actions/bookshelf_actions';
+
+
+export const bookErrorReducer = (state = [], action) => {
+  Object.freeze(state);
+  switch(action.type){
+    case RECEIVE_BOOKSHELF_ERRORS:
+        return action.error;
+    default:
+      return state;
+  }
+
+};
