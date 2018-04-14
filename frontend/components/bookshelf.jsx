a6fee718c2710ffba32d9786784ef67f6f99c27f
appodols <alexander_podolsky@alumni.brown.edu>
2018-04-13 20:37:16 -0400

    correct proportions of bookshelf

--- frontend/components/bookshelf.jsx
+++ frontend/components/bookshelf.jsx
@@ -1,3 +1,4 @@
+
 /*jshint esversion: 6 */
 /*jshint esversion: 6 */
 import React from 'react';
@@ -7,13 +8,7 @@ import StarRatingComponent from 'react-star-rating-component';
 class BookShelf extends React.Component {
   constructor(props){
     super(props);
-    // this.state = {buttonName: "",rating: 0};
-    // this.updateButton = this.updateButton.bind(this);
-    // this.buttonName = this.buttonName.bind(this);
-    // this.deleteUserReview = this.deleteUserReview.bind(this);
-    // this.onStarClick = this.onStarClick.bind(this);
-    // this.onStarHover = this.onStarHover.bind(this);
-    // this.onStarHoverOut = this.onStarHoverOut.bind(this);
+
   }
 
 
@@ -24,46 +19,15 @@ class BookShelf extends React.Component {
     // this.updateButton(this.props);
   }
 
-  // onStarClick(nextValue, prevValue, name) {
-  //
-  //   if(this.props.ownReview){
-  //     const review = {user_id: this.props.currentUser.id, book_id: this.props.book.id, rating: nextValue, id: this.props.ownReview.id};
-  //     this.props.updateReview(review);
-  //   }
-  //   else {
-  //     const review = {user_id: this.props.currentUser.id, book_id: this.props.book.id, rating: nextValue};
-  //     this.props.createReview(review);
-  //   }
-  // }
-
-
-  // updateButton(props){
-  //   let reviewed = this.props.hasReviewed(props.reviews,props.currentUser.id);
-  //   if(reviewed){
-  //     this.setState({buttonName: 'Clear Rating', rating: (props.ownReview ? props.ownReview.rating : 0), initialRating: (props.ownReview ? props.ownReview.rating : 0)});
-  //   } else {
-  //     this.setState({buttonName: 'Rate This Book', rating: 0, initialRating: 0});
-  //   }
-  //
-  // }
 
 
   componentWillReceiveProps(nextProps) {
     if(!this.props.currentBookShelf){
       this.props.fetchBookShelf();
     }
-      // this.updateButton(nextProps);
+
   }
 
-
-   // onStarHover(nextValue, prevValue, name){
-   //   this.setState({rating: nextValue});
-   // }
-   //
-   // onStarHoverOut(nextValue, prevValue, name){
-   //   this.setState({rating: this.state.initialRating});
-   // }
-   //
 
 
 
@@ -73,15 +37,12 @@ class BookShelf extends React.Component {
       <div className ='bookShelf'>
         {this.props.currentBookShelf ?
         <div>
-
-         <header className='header'></header>
+         <header className='bs-header'></header>
          <div className='bookShelfContent'>
            <div className= 'leftColumn'></div>
            <div className= 'rightColumn'></div>
          </div>
-
        </div>
-
         :
            null
          }
