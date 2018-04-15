a28b99786e0e7358e314d887ebce947e699b3784
appodols <alexander_podolsky@alumni.brown.edu>
2018-04-15 15:07:10 -0400

    fixed flexbook issues, added in cover pics in bookshelf

--- frontend/components/shelf_book.jsx
+++ frontend/components/shelf_book.jsx
@@ -7,7 +7,9 @@ import BookRatingContainer from './book_rating_container';
 const ShelfBook = ({book, name}) => {
       return(
           <ul className="bookShelf-book-list">
-            <li><h2 className='bs-cover'>{book.cover}</h2></li>
+            <li>  <img className='bookshelf-cover'
+                src={book.image_url}>
+              </img></li>
             <li><h2 className='bs-title'>{book.title}</h2></li>
             <li><h2 className='bs-author'>{book.author}</h2></li>
             <li><h2 className='bs-avg-rating'>{book.average_rating}</h2></li>
