a7eb2e2d70848325ba5f8f9e243b75644f046fa1
appodols <alexander_podolsky@alumni.brown.edu>
2018-04-20 23:59:19 -0400

    links for bookshelfs in menu

--- frontend/components/Bookshelf/bookshelf_menu.jsx
+++ frontend/components/Bookshelf/bookshelf_menu.jsx
@@ -1,10 +1,14 @@
 /*jshint esversion: 6 */
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const bookshelfMenu = ({bookshelves, allBookShelves}) => {
   return(
     <div className='bookShelfMenu'>
-      yay
+      <Link to={`/bookshelves/all`} className="bookshelf-link">All{allBookShelves[2]}</Link>
+    {bookshelves.map((bookshelf) => {
+      return <Link to={`/bookshelves/${bookshelf[1]}`}className="bookshelf-link">{bookshelf[0]}{bookshelf[2]}</Link>;
+      })}
     </div>
   );
 }
