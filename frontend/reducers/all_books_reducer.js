077fe5e1dfad41bea8718c65abe9c7c60912c7e8
appodols <alexander_podolsky@alumni.brown.edu>
2018-05-02 11:55:17 -0400

    reducer written for new all books slice of state

---
+++ frontend/reducers/all_books_reducer.js
@@ -0,0 +1,15 @@
+/*jshint esversion: 6 */
+import React from 'react';
+import { RECEIVE_BOOKS } from '../actions/book_actions';
+import merge from 'lodash/merge';
+
+
+export const allBookReducer = (state = {} ,action) => {
+  Object.freeze(state);
+  switch(action.type){
+    case RECEIVE_BOOKS:
+      return merge({}, action.books);
+    default:
+      return state;
+  }
+};
