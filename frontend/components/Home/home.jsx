0a7782e25aa7f29d8d7aa12b0fa350f5e89c23ce
appodols <alexander_podolsky@alumni.brown.edu>
2018-05-02 15:11:57 -0400

    home is now functional component

--- frontend/components/Home/home.jsx
+++ frontend/components/Home/home.jsx
@@ -2,25 +2,77 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Home = ({allShelves}) => {
-      return(
-          <div className= 'home-page-container'>
-            {allShelves.length > 0 ?
-            <div className='inner-home-container'>
-                {allShelves.map((book,id) => {
-              return   <Link key={id} className='homepage-cover-link' to={`/books/show/${book.id}`}>
-                    <img className='homepage-cover-img'
-                      key={book.id}
-                      src={book.image_url}>
-                    </img></Link>;
-                })}
-              </div>
-              :
-               null}
 
-         </div>);
-    };
 
+
+class Home extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  componentDidMount(){
+
+  }
+
+  componentWillReceiveProps(nextProps) {
+
+
+  }
+
+
+
+  render(){
+    let allShelves = this.props.allShelves;
+          return(
+              <div className= 'home-page-container'>
+                {allShelves.length > 0 ?
+                <div className='inner-home-container'>
+                    {allShelves.map((book,id) => {
+                  return   <Link key={id} className='homepage-cover-link' to={`/books/show/${book.id}`}>
+                        <img className='homepage-cover-img'
+                          key={book.id}
+                          src={book.image_url}>
+                        </img></Link>;
+                    })}
+                  </div>
+                  :
+                   null}
+
+             </div>);
+        };
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const Home = ({allShelves, allBooks}) => {
+//       return(
+//           <div className= 'home-page-container'>
+//             {allShelves.length > 0 ?
+//             <div className='inner-home-container'>
+//                 {allShelves.map((book,id) => {
+//               return   <Link key={id} className='homepage-cover-link' to={`/books/show/${book.id}`}>
+//                     <img className='homepage-cover-img'
+//                       key={book.id}
+//                       src={book.image_url}>
+//                     </img></Link>;
+//                 })}
+//               </div>
+//               :
+//                null}
+//
+//          </div>);
+//     };
+//
 
 // <Link className= 'addaReview'to='/reviews/new'>Add a review</Link>
 
