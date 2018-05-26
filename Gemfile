708acc4956d1d79a750cc7750736b390ac2f7d8a
appodols <alexander_podolsky@alumni.brown.edu>
2018-05-26 00:02:02 -0400

    removes logger

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
-gem 'pry-rails'
+# gem 'pry-rails'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.5'
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,73 @@
+/*jshint esversion: 6 */
 
-var path = require("path");
+//
+// var path = require("path");
+//
+// module.exports = {
+//   context: __dirname,
+//   entry: "./frontend/entry.jsx",
+//   output: {
+//     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+//     filename: "bundle.js"
+//   },
+//   module: {
+//     loaders: [
+//       {
+//         test: [/\.jsx?$/, /\.js?$/],
+//         exclude: /node_modules/,
+//         loader: 'babel-loader',
+//         query: {
+//           presets: ['env', 'react']
+//         }
+//       }
+//     ]
+//   },
+//   devtool: 'source-map',
+//   resolve: {
+//     extensions: [".js", ".jsx", "*"]
+//   }
+// };
+//
+
+
+const path = require('path');
+const webpack = require('webpack');
+
+let plugins = [];
+let devPlugins = [];
+
+let prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+);
 
 module.exports = {
   context: __dirname,
   entry: "./frontend/entry.jsx",
   output: {
     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
-    filename: "bundle.js"
+    filename: 'bundle.js'
+  },
+  plugins: plugins,
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
   },
   module: {
     loaders: [
       {
-        test: [/\.jsx?$/, /\.js?$/],
-        exclude: /node_modules/,
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
         loader: 'babel-loader',
         query: {
           presets: ['env', 'react']
@@ -20,8 +75,5 @@ module.exports = {
       }
     ]
   },
-  devtool: 'source-map',
-  resolve: {
-    extensions: [".js", ".jsx", "*"]
-  }
+  devtool: 'source-map'
 };
