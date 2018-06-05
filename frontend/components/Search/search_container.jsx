510eb45c6a96025c696cbc0b9ddd90d27d34d731
appodols <alexander_podolsky@alumni.brown.edu>
2018-06-05 17:15:14 -0400

    search-container

---
+++ frontend/components/Search/search_container.jsx
@@ -0,0 +1,21 @@
+/*jshint esversion: 6 */
+import { connect } from 'react-redux';
+import Search from './search';
+import {updateSearchQuery} from '../actions/search';
+import {searchSelector } from '../reducers/selectors';
+
+const msp = (state) => {
+
+  return({
+    searchQuery: searchSelector(state)
+  });
+};
+
+const mdp = (dispatch) => {
+  return({
+    updateSearchQuery: (query) => dispatch(updateSearchQuery(query))
+  });
+};
+
+
+export default connect (msp,mdp)(Search);
